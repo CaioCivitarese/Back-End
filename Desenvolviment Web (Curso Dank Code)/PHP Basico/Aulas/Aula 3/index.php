343c732b61f777db58af2e74befe7aff122c9c75
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2025-12-10 10:35:06 -0300

    add

---
+++ Desenvolviment Web (Curso Dank Code)/PHP Basico/Aulas/Aula 3/index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+    $nome = "Caio";
+    echo "meu nome e <br />";
+    echo $nome;
+    $nome + "João";
+    echo "<br />";
+    echo $nome;
+*/
+    //string
+    $nome = "Caio";
+    //Valor inteiro
+    $idade = 19;
+    echo $idade;
+?>

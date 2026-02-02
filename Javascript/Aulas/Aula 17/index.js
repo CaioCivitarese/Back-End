fee754b39d732286bc2c5998885cab6669af75a5
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-02-02 14:34:02 -0300

    add

---
+++ Javascript/Aulas/Aula 17/index.js
@@ -0,0 +1,45 @@
+/*
+function saudacao(nome){
+   return `Bom Dia ${nome}!`;
+}
+
+const varivel = saudacao("Caio");
+console.log(varivel);
+*/
+/*
+function some (x = 1, y = 1){
+    const resultado = x + y;
+    return resultado;
+    console.log('Código após o return'); // Nunca será executado
+}
+
+const resultado =  some();
+console.log(resultado);
+*/
+/*
+console.log(soma(2, 2));
+console.log(soma(3, 1));
+console.log(soma(5, 10));
+*/
+
+/*
+console.log(some(2, 2));
+console.log(resultado);
+*/
+
+/*
+const raiz = function(n){
+    return n ** 0.5;
+
+};
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+*/
+
+const  raiz = (n) => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
---
+++ Javascript/Exercicio de Fixação/Ex 1/Index.js
@@ -0,0 +1,16 @@
+function soma(x, y){
+    const resultado = x + y;
+    return resultado;
+
+}
+
+function sub(x, y){
+    const resultado = x - y;
+    return  resultado;
+
+}
+
+const resultadoSoma = soma(5, 3);
+console.log("Resultado da soma: " + resultadoSoma);
+const resultadoSub = sub(10, 4);
+console.log("Resultado da subtração: " + resultadoSub);

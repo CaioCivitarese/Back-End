9a59cb32297f6c06eaf65ca698fc96817cbfbb8f
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-02-02 12:00:43 -0300

    add

--- Javascript/Aulas/Aula 15/js/index.js
+++ Javascript/Aulas/Aula 15/js/index.js
@@ -1,1 +1,11 @@
-const num1 = prompt("Digite um numero:");
+const num1 = Number(prompt("Digite um numero: "));
+const numeroTitulo = document.getElementById("numero-titulo");
+const texto = document.getElementById("texto");
+
+numeroTitulo.innerHTML = num1;
+texto.innerHTML += `<p>Seu numero + 2 e ${num1 + 2}.</p>`;
+texto.innerHTML +=  `${num1} e inteiro :  ${Number.isInteger(num1)}. <br/>`;
+texto.innerHTML += `e NaN : ${Number.isNaN(num1)}. <br/>`;
+texto.innerHTML += `Arredondando para cima: ${Math.ceil(num1)}. <br/>`;
+texto.innerHTML += `Arredonadando para baixo: ${Math.floor(num1)}. <br/>`;
+texto.innerHTML += `Com duas casas decimais: ${num1.toFixed(2)}. <br/>`;
---
+++ Javascript/Aulas/Aula 16/index.js
@@ -0,0 +1,48 @@
+const alunos = ["Luiz", "Maria", "João"]; //Luiza, Eduardo
+
+//console.log(typeof alunos);
+//console.log(alunos instanceof Array); //Verifica se é um array
+
+//alunos.push("Luiza"); //Adiciona no final do array
+//alunos.push("Eduardo");
+
+//console.log(alunos.slice(0, 3));
+
+
+//console.log(alunos[50]);
+
+
+//delete alunos[1]; //Deleta o índice 1, mas mantém o índice no array (undefined)
+
+//console.log(alunos);
+
+//const removido = alunos.pop();
+//const removido2 = alunos.shift();
+
+//console.log(removido);
+//console.log(removido2);
+//console.log(alunos);
+
+//alunos.unshift("Luiza"); //Adiciona no início do array
+//alunos.unshift("Fabio");
+
+c//onsole.log(alunos);
+
+//alunos.push("Luiza");
+//alunos.push("Fabio");
+
+//console.log(alunos);
+
+
+
+//alunos[alunos.length] = "Luiza";
+//alunos[alunos.length] = "Fabio";
+//alunos[alunos.length] = "Luana";
+//console.log(alunos);
+
+//alunos[0] = "Eduardo"; //Alterando o valor do índice 0
+//alunos[3] = "Luiza";
+
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]);

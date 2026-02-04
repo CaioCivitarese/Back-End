d640c181fd97d587cf56d5bfc1d1cb47d1f6cec7
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-02-04 18:20:16 -0300

    add

---
+++ Javascript/Aulas/Aula 21/index.js
@@ -0,0 +1,28 @@
+/*
+Operadores de comparação
+> maior que
+< menor que
+>= maior ou igual a
+<= menor ou igual a
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+const comp = 10 > 5;
+const comp2 = 10 >= 5;
+const comp3 = 10 <= 11;
+const comp4 = 10 < 11;
+const comp5 = 10 == '10';
+const comp6 = 10 === 10;
+const comp7 = 10 != '11';
+const comp8 = 10 !== 11;
+
+console.log(comp);
+console.log(comp2);
+console.log(comp3);
+console.log(comp4);
+console.log(comp5);
+console.log(comp6);
+console.log(comp7);
+console.log(comp8);

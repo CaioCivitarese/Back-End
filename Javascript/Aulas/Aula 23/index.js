f4ae68939f2e71988dfb7c8eb8d4ee4d705b5d67
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-02-07 15:03:16 -0300

    add

--- Javascript/Aulas/Aula 23/index.js
+++ Javascript/Aulas/Aula 23/index.js
@@ -1,5 +1,40 @@
 /*
 && -> false && true -> false
-|| -> 
+FALSY
+*false
+0
+"" '' ``
+null
+undefined
+NaN
 */
-console.log('luiz Oltavio' && 0 && 'Maria');
+
+/*
+function falaOi(){
+    return 'Oi';
+}
+
+let vaiExecutar = 'Joãozinho';
+
+console.log(vaiExecutar && falaOi());
+*/
+
+/*
+|| -> true && false -> vai retornar "o valor verdadeira"
+*/
+
+//console.log(0 || false ||null || 'Luiz Otavio' || true);
+
+/*
+const corUsuario = 'Vermelho';
+const corPadrao = corUsuario || 'preto';
+console.log(corPadrao);
+*/
+
+const a = 0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e);

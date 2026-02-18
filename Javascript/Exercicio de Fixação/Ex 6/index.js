6d1ef281b5f45ad877084507773ab89f887d1256
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-02-18 08:50:21 -0300

    add

---
+++ Javascript/Exercicio de Fixação/Ex 6/index.js
@@ -0,0 +1,11 @@
+/*
+    3️⃣ Dobro de número
+
+    Crie uma função:
+
+    function dobro(n) {}
+
+
+    Retorne o dobro.
+*/
+
---
+++ Javascript/Exercicio de Fixação/Ex 8/index.js
@@ -0,0 +1,11 @@
+/*
+    4️⃣ Nome grande ou pequeno
+
+    Peça um nome e diga:
+
+    Nome curto (≤ 4 letras)
+
+    Nome normal
+
+    Nome grande (≥ 10)
+*/

983db495209942ac8cc9b9b6042380eccbf20609
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-02-23 19:45:22 -0300

    add

---
+++ Javascript/exercicio para live/Ex 1/index.js
@@ -0,0 +1,23 @@
+/*
+1. Adivinhe o número
+Criar um programa que tenta descobrir um número escolhido pelo público dentro de um intervalo definido.
+O algoritmo deve interagir com respostas do tipo maior ou menor até chegar ao valor correto.
+ Dica:
+Pense em estratégias que reduzem o número de tentativas. Nem sempre testar em ordem é a melhor escolha.
+
+obs:
+    intervalo: 0 a 10;
+*/
+
+const num = 0;
+
+function numSelct(){
+    const n = num;
+    const alt = 
+    if (n === ){
+        console.log("seu numero esta enter");
+    }
+}
+
+const resp = numSelct();
+console.log(resp);

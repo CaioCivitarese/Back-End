bdeeeb15291f954b5659322c5ada286ae272a38c
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-03-18 16:01:19 -0300

    add

--- Javascript/Aulas/Aula 47/assets/js/main.js
+++ Javascript/Aulas/Aula 47/assets/js/main.js
@@ -1,6 +1,6 @@
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausae');
+const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 
 iniciar.addEventListener('click', function (event){

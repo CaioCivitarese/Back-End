7041f50678ef37fed7890eaa5a5427f1bc5caf35
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-03-18 17:45:06 -0300

    add

--- Javascript/Exercicio de Fixação/Ex 7/index.js
+++ Javascript/Exercicio de Fixação/Ex 7/index.js
@@ -9,3 +9,23 @@
 
     Nome grande (≥ 10)
 */
+
+let nome = "Caio".length;
+
+
+console.log(nome)
+
+function numeroDELetras (){
+    let n = nome;
+    if (n <= 4){
+        return 'Nome pequeno';
+    }else if (n >= 5 && n <= 9){
+        return "Nome normal";
+    }else if (n >= 10){
+        return 'Nome grande';
+    }else {
+        return 'Nome invalido';
+    }
+}
+
+console.log(numeroDELetras());

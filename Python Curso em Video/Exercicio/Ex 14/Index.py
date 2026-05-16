8d1df6a447619bd7c3f6c069082d03263eaa9a59
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-05-16 06:47:04 -0300

    add

---
+++ Python Curso em Video/Exercicio/Ex 14/Index.py
@@ -0,0 +1,6 @@
+# Ecreva um programa que converta uma teperatura digitada em °C e converta para °F.
+
+n1 = int(input('Escreva sua temperatura em ceucios: '))
+converter = ((9 * n1) / 5) + 32
+
+print(converter)

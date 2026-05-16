d8fb4fda2d91fd790f5003c92bee36e0dd486dae
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-05-16 07:06:40 -0300

    add

--- Python Curso em Video/Exercicio/Ex 15/index.py
+++ Python Curso em Video/Exercicio/Ex 15/index.py
@@ -1,3 +1,10 @@
 # Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado
 
-p1 = int(input('Escreva quantos km vc rodou'))
+p1 = int(input('Escreva quantos km vc rodou: '))
+d1 = float(input('Escreva quantos dias vc rodou: '))
+
+diasResultado = d1 * 60
+kmRodado = p1 * 0.15
+totalRodado = diasResultado + kmRodado
+
+print('O total a pagar e R${}'.format(totalRodado))

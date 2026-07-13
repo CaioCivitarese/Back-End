14fb6f830191db1f025f8d028590b1711a482101
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-07-12 21:47:20 -0300

    add

---
+++ Javascript/200 exercicios de Javascript/exercicios/Ex 12/index.js
@@ -0,0 +1,9 @@
+/*
+Exercício 12:
+    Escreva um programa que verifica a situação de um estudante
+    de acordo com sua média final.
+Descrição: 
+    Neste  exercício, você vai criar uma função que recebe a média final de um
+    estudante e verifica sua situação, se aprovado (média igual ou superior a 7), se em
+    recuperação (média entre 5 e 6.9) ou se reprovado (média abaixo de 5)
+*/

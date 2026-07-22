ed533579ad2141e29c93c18200cf2df03344b7fc
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-07-22 11:37:07 -0300

    add

---
+++ Curso IA na Copa Puc Rio/Exercicios/Ex 1/Ex 1.py
@@ -0,0 +1,10 @@
+# Declare duas variáveis e atribua valores numéricos a elas. Use
+# o operador de adição para realizar operações entre as duas variáveis.
+# Imprima o resultado.
+
+comida = 5
+bebida = 3
+
+soma = comida + bebida
+
+print(soma)
---
+++ Curso IA na Copa Puc Rio/Exercicios/Ex 3/index.py
@@ -0,0 +1,8 @@
+num = 1
+num2 = 100
+
+for c in range(num2):
+    num += 1
+
+
+print(num)

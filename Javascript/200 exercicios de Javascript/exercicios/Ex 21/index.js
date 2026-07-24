7f6b2cfde30aac5fb3500909e9799afcf5e7015c
CAIO FARIA CIVITARESE <caiofariacivitarese@gmail.com>
2026-07-24 18:09:00 -0300

    add

--- Javascript/200 exercicios de Javascript/exercicios/Ex 21/index.js
+++ Javascript/200 exercicios de Javascript/exercicios/Ex 21/index.js
@@ -8,4 +8,21 @@ Descrição:
     correspondente.
 */
 
+function estacaoDoAno(num){
+    if(num === 1 && num === 2 && num === 12){
+        console.log("Verão");
 
+    }else if(num === 3 && num === 4 && num === 5){
+        console.log("Outono");
+
+    }else if(num === 6 && num === 7 && num === 8){
+        console.log("Inverno");
+
+    }else if(num === 9 && num === 10 && num === 11){
+        console.log("Primavera");
+    }else{
+        console.log("Valor solicitado invalido");
+    }
+}
+
+estacaoDoAno(11);
--- Javascript/200 exercicios de Javascript/exercicios/Ex 22/index.js
+++ Javascript/200 exercicios de Javascript/exercicios/Ex 22/index.js
@@ -6,3 +6,5 @@ Descrição:
     Neste exercício, você criará uma função que recebe o número de um mês e
     imprime o número de dias desse mês.
 */
+
+
